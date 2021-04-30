2d2f3ab6cbf509b7327efa9ae9d41ab3eb898338
aminaligholi <amin.aligholi@gmail.com>
2021-05-01 00:28:40 +0430

    update 2

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import VueRouter from 'vue-router'
 import {router} from './router';
 import VueSweetalert2 from 'vue-sweetalert2';
 import 'sweetalert2/dist/sweetalert2.min.css';
-import "@/assets/Css/global.css";
+import '../public/Css/global.css'
 
 Vue.use(VueRouter)
 Vue.use(VueSweetalert2);
